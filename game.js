0e9da0b64b434931a418a3d2a737e399ab497a97
I-am-copy-coder <himanshu_s@ch.iitr.ac.in>
2022-01-31 05:43:46 +0530

    Button Click Detection Added

--- game.js
+++ game.js
@@ -1,20 +1,30 @@
 // alert("JS file linked");
-
+let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red","blue","green","yellow"];
 
+// function nextSequence() {
+//     let randomNumber = Math.floor(Math.random()*4);
+//     let randomChosenColor = buttonColors[randomNumber];
+//     gamePattern.push(randomChosenColor);
 
+//     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+//     var sound = new Audio(`sounds/${randomChosenColor}.mp3`);
+//     sound.play();
+// }
 
 let randomNumber = Math.floor(Math.random()*4);
 let randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChosenColor);
 
-
-
 $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
 var sound = new Audio(`sounds/${randomChosenColor}.mp3`);
 sound.play();
 
-
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+})
 
 // alert(randomChosenColor);
