3e7eb60dca410759a38c4a12bacb6d7c3c6b3d2b
I-am-copy-coder <himanshu_s@ch.iitr.ac.in>
2022-01-31 05:49:22 +0530

    Sound added on button click

--- game.js
+++ game.js
@@ -3,6 +3,12 @@ let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red","blue","green","yellow"];
 
+function playSound(name) {
+    var sound = new Audio("sounds/"+name+".mp3");
+    sound.play();
+}
+
+
 // function nextSequence() {
 //     let randomNumber = Math.floor(Math.random()*4);
 //     let randomChosenColor = buttonColors[randomNumber];
@@ -18,13 +24,16 @@ let randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChosenColor);
 
 $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
-var sound = new Audio(`sounds/${randomChosenColor}.mp3`);
-sound.play();
+playSound(randomChosenColor);
+
 
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
     console.log(userClickedPattern);
 })
+
+
 
 // alert(randomChosenColor);
